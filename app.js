38582442136ea5a047fc5eb7378ea0d4e85c5297
Michael Tambornino <michael.tambornino@gmail.com>
2017-05-05 10:00:59 -0500

    commenting

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ const rp = require('request-promise')
 const { writeFile } = require('fs')
 const { factory } = require('./factory.js')
 
-const results = 4 // set however many randos you want
+const results = 1000 // the api returns a max of 5000 users
 
 const getRandos = () => {
-  rp({
+  rp({ // make http call
     uri: `https://randomuser.me/api/?nat=us&results=${results}&inc=name,email,location,picture`,
     json: true
   })
-  .then((data) => {
+  .then((data) => { // send to factory for any alterations
     return factory(data.results)
   })
-  .then((newData) => {
-    strungData = JSON.stringify(newData, null, '\t')
-    console.log(strungData)
+  .then((newData) => { // stringify and write to json file
+    strungData = JSON.stringify(newData) // add parameters null and '\t' to prettify
     writeFile('randomUsers.json', strungData, function(err) {
       err ? console.log(err) : console.log("Random users saved to json")
     })
--- factory.js
+++ factory.js
@@ -1,8 +1,8 @@
 'use strict'
+const instruments = require('./instruments.json')
 
-// perform map methods to rearrange the json
-
-module.exports.factory = (array) => {
-  console.log(`arranging...`)
+module.exports.factory = (array) => { // perform map methods to add a random instrument to each user
+  console.log(`adding instruments...`)
+  array.map(x => x.instrument = instruments[Math.floor(Math.random() * instruments.length)])
   return array
 }
